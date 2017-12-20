0a8145c8a9cc0295e9732b01b431702c3c5e9236
Unknown <gw9225@vt.edu>
2017-12-19 20:07:08 -0500

    ADC_1219 added

---
+++ ADC_1219/ADC_1219.ino
@@ -0,0 +1,39 @@
+#include <MCP3008.h>
+
+#define INTERVAL_LENGTH_US 150UL   //fixed sampling set-up
+unsigned long previousMicros;
+
+//define pin connections
+#define CS_PIN 12
+#define CLOCK_PIN 9
+#define MOSI_PIN 11
+#define MISO_PIN 10
+
+MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
+
+void setup()
+{
+  // open serial port
+  Serial.begin(2000000);
+}
+
+void loop() {
+
+  unsigned long currentMicros = micros();
+
+  if ((currentMicros - previousMicros) >= INTERVAL_LENGTH_US)
+    {
+        previousMicros += INTERVAL_LENGTH_US;
+          
+  int pot1 = adc.readADC(0);
+  int pot2 = adc.readADC(2);
+  Serial.print(pot1);
+  Serial.print(" ");
+  Serial.print(pot2);
+  Serial.print(" ");
+  Serial.print(((double)currentMicros) / 1000000UL, 5);
+  Serial.println();
+    }
+
+}
+
